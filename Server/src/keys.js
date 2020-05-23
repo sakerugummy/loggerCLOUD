d737d8a3475087b173b241fb0538620a143aab4c
sakerugummy <sakerugummy@hotmail.com>
2020-05-23 16:44:31 -0400

    up1

--- Server/src/keys.js
+++ Server/src/keys.js
@@ -1,9 +1,9 @@
 module.exports = {
     database: {
         connectionLimit: 10,
-        host: process.env.DATABASE_HOST || '206.189.201.180',
-        user: process.env.DATABASE_USER || 'admin',
-        password: process.env.DATABASE_PASSWORD || 't1sk04DM1N.2040',
+        host: process.env.DATABASE_HOST || 'localhost',
+        user: process.env.DATABASE_USER || 'userIOT',
+        password: process.env.DATABASE_PASSWORD || 'userIOT.123',
         database: process.env.DATABASE_NAME || 'db_loggerCLOUD'
     }
 
